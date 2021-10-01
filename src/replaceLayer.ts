81c303c7978ac3a2a990c3958a2ccc683b5268f0
Erik Lundh <elundh@mutulaofenumclaw.com>
2021-10-01 16:31:44 -0700

    NPL-1387 add comments, remove call to secrets manager not needed.

--- src/replaceLayer.ts
+++ src/replaceLayer.ts
@@ -3,6 +3,10 @@ import { Lambda, SecretsManager } from 'aws-sdk';
 const lambda = new Lambda();
 const secrets = new SecretsManager();
 
+/**
+ * This lambda triggers when a layer is updated/added to cloudwatch  it checks to see if the description
+ * conains auto update. If so it checks every lambda function to see if that layer is used and updates the layer version.
+ */
 module.exports.eventHandler = async (event) => {
     console.log(JSON.stringify(event));
 
@@ -11,17 +15,19 @@ module.exports.eventHandler = async (event) => {
     if(event.detail.responseElements.description.indexOf('auto update') < 0) {
         return;
     }
-
+    // get the prefix which is the arn without the layer version
     let lastColon = layerArn.lastIndexOf(':') + 1;
     const prefix = layerArn.slice(0, lastColon - 1);
     console.log(`prefix: ${prefix}`);
     lastColon = prefix.lastIndexOf(':') + 1;
-
+    
+    // construct the path to store the most current version of the layer in secrets manager
     const key = `layers/${layerArn.slice(lastColon, prefix.length)}`;
     console.log(`Getting layer reference ${key}`);
 
     let layerSecret;
     
+    // look up the arn
     try {
         layerSecret = await secrets.describeSecret({
             SecretId: key,
@@ -36,6 +42,8 @@ module.exports.eventHandler = async (event) => {
     const value = JSON.stringify({
         latest: event.detail.responseElements.layerVersionArn
     });
+
+    // if no secret exists for the layer add one, otherwise update it
     if(!layerSecret) {
         console.log('Creating secret')
         await secrets.createSecret({
@@ -75,15 +83,21 @@ module.exports.eventHandler = async (event) => {
             }
             let layers = item.Layers.map(layer => layer.Arn);
             const index = layers.findIndex(layer => layer.startsWith(prefix));
+            
+            // if none of the functions layers match the layer we are updating there is nothing left to do
             if(index < 0) {
                 return;
             }
             console.log(`Applying new version of layer to lambda ${item.FunctionName}`);
             console.log('Layers Before', layers);
+            
+            // update the layer to the new version if it matches the layer being updated
             layers = await Promise.all(layers.map((arn) => {
                 if (arn.startsWith(prefix)) {
                     console.log(`Getting latest layer for ${arn} in ${item.FunctionName}`);
-                    return getLatestLayer(arn, lookup);
+                    // return getLatestLayer(arn, lookup);
+                    // no need to lookup the arn in secrets manager we have it already.
+                    return layerArn;
                 }
                 else {
                     console.log(`Not replacing layer ${arn} in ${item.FunctionName}`);
@@ -94,10 +108,12 @@ module.exports.eventHandler = async (event) => {
             console.log('Layers', layers);
             console.log(item.Layers.length);
 
+            // check to make sure none of the layer arns returned from the map are undefined 
             if(layers.filter(x => x? true : false).length != item.Layers.length) {
                 throw new Error(`Layers after processing don't match before processing (${layers})(${item.Layers})`);
             }
 
+            // update the function with the updated layers
             try {
                 await lambda.updateFunctionConfiguration({
                     FunctionName: item.FunctionName,
